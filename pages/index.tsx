8c2be77f8f276b2d9f5f587e055bfb1bd5114acd
김민혜 <kmhkgh@naver.com>
2022-08-29 09:40:20 +0900

    Test: 테스트 코드

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Layout>
-        <Box>fontWeight Test</Box>
-        <Box fontWeight={700}>bold - 700</Box>
-        <Box fontWeight={500}>medium - 500</Box>
-        <Box fontWeight={400}>normal - 400</Box>
-        <Box fontWeight={200}>light - 300</Box>
+        <Box>테스트</Box>
       </Layout>
     </>
   );
