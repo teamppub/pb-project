33112cc3b32d6b6469414da593241b90eebfd08b
mndangee <38105502+mndangee@users.noreply.github.com>
2022-09-06 08:14:08 +0900

    git 서버 셋팅 환경 설정 (#4)
    * setting-host
    
    * setting-host
    
    * setting-host
    
    * Deploy Next.js to gh-pages
    
    * Deploy Next.js to gh-pages
    
    * setting-host
    
    * hosting setting -ing

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? 'https://teamppub.github.io/pb-project'
+      : '',
 };
 
 module.exports = nextConfig;
