fb7925d0ed0bc775b3e5afd91671161e017bef3f
LAPTOP-OS827ECQ\dldbs <dldbsrud0925@gmail.com>
2022-09-05 15:19:05 +0900

    lists

--- components/Organisms/List/BookList.tsx
+++ components/Organisms/List/BookList.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 
-import { Box, FlexBox, List, Span } from 'components/Atoms';
 import BookItem from 'components/Organisms/List/BookItem';
 import { data } from 'components/Organisms/List/data';
 
@@ -15,39 +14,6 @@ export default function BookList(props: BookCateProps) {
   const subJect = bookData.lists.filter(
     (subject) => subject.subject === bookList,
   );
-
-  // 랜덤 인덱스
-  const randomIndex = () => {
-    return Math.floor(Math.random() * bookData.lists.length);
-  };
-
-  const makeNewBookList = () => {
-    const list = [];
-    // for (let i = 0; i < bookData.lists.length; i++) {
-    //   const NewBookList = bookData.lists[randomIndex()];
-    //   if (list.indexOf(NewBookList) === -1) {
-    //     console.log(list);
-    //     list.push(NewBookList);
-    //   }
-    // }
-
-    //   while (list.length !== 28) {
-    //     const NewBookList = bookData.lists[randomIndex()];
-    //     if (list.indexOf(NewBookList) === -1) {
-    //       console.log(list);
-    //       list.push(NewBookList);
-    //     }
-    //   }
-    //   return list;
-  };
-
-  console.log(makeNewBookList());
-
-  useEffect(() => {
-    // const list = [];
-    // list.push(makeNewBookList());
-    // setNewBookData();
-  }, []);
 
   if (bookList === '전체') {
     return (
