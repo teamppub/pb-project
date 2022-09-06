6ec13dcbf6a5803f617d089f43695ad95ac96cf9
LAPTOP-OS827ECQ\dldbs <dldbsrud0925@gmail.com>
2022-09-06 13:10:35 +0900

    랜덤 리스트 만들기

--- components/Organisms/List/BookList.tsx
+++ components/Organisms/List/BookList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 
 import BookItem from 'components/Organisms/List/BookItem';
@@ -9,11 +10,24 @@ interface BookCateProps {
 
 export default function BookList(props: BookCateProps) {
   const bookList = props.bookCate;
-  const [bookData, setNewBookData] = useState(data);
+  const bookData = data;
+  const [NewData, setNewBookData] = useState(data);
 
   const subJect = bookData.lists.filter(
     (subject) => subject.subject === bookList,
   );
+
+  // const makeRandomList = () => {
+  //   return bookData.lists.sort(() => Math.random() - 0.5);
+  // };
+
+  // 렌더링 될 때마다 실행됨
+  useEffect(() => {
+    const makeRandomList = () => {
+      return bookData.lists.sort(() => Math.random() - 0.5);
+    };
+    setNewBookData(makeRandomList, []);
+  }, []);
 
   if (bookList === '전체') {
     return (
