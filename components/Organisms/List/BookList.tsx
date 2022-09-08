1ccc992a2d99fd4a2a1dba4da4c219ef51dde757
LAPTOP-OS827ECQ\dldbs <dldbsrud0925@gmail.com>
2022-09-08 10:52:33 +0900

    랜덤 리스트 수정

--- components/Organisms/List/BookList.tsx
+++ components/Organisms/List/BookList.tsx
@@ -8,26 +8,22 @@ interface BookCateProps {
   bookCate: string;
 }
 
-// random 리스트
-const Random = () => {
-  return Math.random() - 0.5;
-};
-
-const Randomed = [...data.lists].sort(Random);
-
 export default function BookList(props: BookCateProps) {
   const bookList = props.bookCate;
   const [NewData, setNewBookData] = useState(data.lists);
-  const RandomListRef = useRef(Randomed);
+
+  const Randomed = [...data.lists].sort(() => {
+    return Math.random() - 0.5;
+  });
 
   const subJect = data.lists.filter((subject) => subject.subject === bookList);
 
   useEffect(() => {
     const makeRandomList = () => {
-      return RandomListRef.current;
+      return Randomed;
     };
     setNewBookData(makeRandomList);
-  }, [bookList]);
+  }, []);
 
   if (bookList === '전체') {
     return (
