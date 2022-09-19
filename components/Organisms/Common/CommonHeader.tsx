3f86b925669184c30776037d431df814352b8b75
김민혜 <kmhkgh@naver.com>
2022-09-19 16:06:07 +0900

    Refactor: 헤더 position 변경

--- components/Organisms/Common/CommonHeader.tsx
+++ components/Organisms/Common/CommonHeader.tsx
@@ -5,19 +5,12 @@ import { useRef } from 'react';
 import iconBack from 'assets/Images/common/ico_back.svg';
 import { Box } from 'components/Atoms';
 import theme from 'styles/theme';
-import useRefUtils from 'utils/hooks/useRefUtils';
 
 export default function CommonHeader() {
   const router = useRouter();
-  const { ref, height } = useRefUtils();
   return (
     <>
-      <Box
-        ref={ref}
-        position="fixed"
-        background={theme.colors.white}
-        height="45px"
-      >
+      <Box position="relative" background={theme.colors.white} height="45px">
         <Box paddingLeft="15px" onClick={() => router.back()}>
           <Image
             src={iconBack}
@@ -28,7 +21,6 @@ export default function CommonHeader() {
           />
         </Box>
       </Box>
-      <Box height={height} />
     </>
   );
 }
