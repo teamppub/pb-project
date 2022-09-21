a3af84e7b1e7281a617e4fe12937ed37af8be533
LAPTOP-OS827ECQ\dldbs <dldbsrud0925@gmail.com>
2022-09-21 13:31:49 +0900

    변수명 표기

--- components/Organisms/List/BookList.tsx
+++ components/Organisms/List/BookList.tsx
@@ -9,22 +9,22 @@ interface BookCateProps {
 
 export default function BookList(props: BookCateProps) {
   const bookList = props.bookCate;
-  const [NewData, setNewBookData] = useState(data.lists);
+  const [newData, setNewBookData] = useState(data.lists);
 
-  const Randomed = [...data.lists].sort(() => {
+  const randomed = [...data.lists].sort(() => {
     return Math.random() - 0.5;
   });
 
   const subJect = data.lists.filter((subject) => subject.subject === bookList);
 
   useEffect(() => {
-    setNewBookData(Randomed);
+    setNewBookData(randomed);
   }, []);
 
   if (bookList === '전체') {
     return (
       <ul className="ListBox">
-        {NewData.map((list) => (
+        {newData.map((list) => (
           <BookItem list={list} key={list.id} />
         ))}
       </ul>
