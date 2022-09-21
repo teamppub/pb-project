8188cbe1443686480a64072ff2b181b9efce2ee3
LAPTOP-OS827ECQ\dldbs <dldbsrud0925@gmail.com>
2022-09-21 13:00:02 +0900

    bottom nav search add, mypage delete

--- assets/common/bottomNavigator/NavSearch.tsx
+++ assets/common/bottomNavigator/NavSearch.tsx
@@ -1,6 +1,6 @@
-export default function NavMyPage({
-  width = '30',
-  height = '30',
+export default function NavSearch({
+  width = '28',
+  height = '28',
   fill = 'none',
 }) {
   return (
@@ -8,26 +8,25 @@ export default function NavMyPage({
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
-      viewBox="0 0 30 30"
+      viewBox="0 0 50 50"
     >
       <path data-name="사각형 9" fill="none" d="M0 0h30v30H0z" />
       <g data-name="그룹 1524">
         <path
           data-name="패스 1486"
-          d="M13.388 4.9A4.4 4.4 0 1 1 8.985.5a4.4 4.4 0 0 1 4.403 4.4z"
-          transform="translate(6.014 3.801)"
+          d="M21 4A16 16 0 1 0 21 36A16 16 0 1 0 21 4Z"
           stroke="#000"
+          strokeLinecap="round"
           strokeMiterlimit="10"
-          strokeWidth="1.2px"
+          strokeWidth="2"
           fill={fill}
         />
         <path
           data-name="패스 1487"
-          d="M.5 17.922A10.361 10.361 0 0 1 11.065 7.78 10.361 10.361 0 0 1 21.63 17.922"
-          transform="translate(3.935 7.899)"
+          d="M32.229 32.229L45.5 45.5"
           stroke="#000"
           strokeMiterlimit="10"
-          strokeWidth="1.2px"
+          strokeWidth="4"
           fill={fill}
         />
       </g>
--- components/Organisms/Common/Container.tsx
+++ components/Organisms/Common/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 import NavHome from 'assets/common/bottomNavigator/NavHome';
 import NavList from 'assets/common/bottomNavigator/NavList';
-import NavMyPage from 'assets/common/bottomNavigator/NavMyPage';
+import NavSearch from 'assets/common/bottomNavigator/NavSearch';
 import NavWish from 'assets/common/bottomNavigator/NavWish';
 import { Box } from 'components/Atoms';
 import BottomNavigator from 'components/Organisms/Common/BottomNavigator';
@@ -29,8 +29,8 @@ export default function Container({ children }: { children: ReactNode }) {
             path: ['/'],
           },
           {
-            selectedIcon: <NavMyPage fill={theme.colors.black} />,
-            icon: <NavMyPage />,
+            selectedIcon: <NavSearch fill={theme.colors.black} />,
+            icon: <NavSearch />,
             pathName: 'SEARCH',
             path: ['/search'],
           },
